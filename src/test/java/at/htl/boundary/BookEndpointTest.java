23afbe492cee64abbbb72c14d7978605d5abfd1d
floklausner <klausnerflo@gmail.com>
2022-09-11 20:18:09 +0200

    Klausner: added Endpoint Tests

--- src/test/java/at/htl/boundary/BookEndpointTest.java
+++ src/test/java/at/htl/boundary/BookEndpointTest.java
@@ -2,9 +2,14 @@ package at.htl.boundary;
 
 import at.htl.entity.Author;
 import at.htl.entity.Book;
+import io.agroal.api.AgroalDataSource;
 import io.quarkus.test.junit.QuarkusTest;
+
+import io.restassured.http.ContentType;
+import org.assertj.db.type.Table;
 import org.junit.jupiter.api.Test;
 
+import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +21,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
 class BookEndpointTest {
+
+    @Inject
+    AgroalDataSource dataSource;
 
     @Test
     void getAllBooks() {
@@ -34,13 +42,56 @@ class BookEndpointTest {
         assertThat(bookList)
                 .isNotEmpty()
                 .hasSize(2)
-                .extracting(book -> book.getAuthor().getAge())
-                .contains(19,19);
+                .extracting(book -> book.getAuthor().getName())
+                .contains("Florian Klausner","Rosalie Mandel");
 
     }
 
     @Test
     void addToBookStore() {
+
+        //Arrange
+        Book book = new Book("Mamamia", 16.99, null, null);
+
+        Table table = new Table(dataSource, "BOOK");
+        org.assertj.db.output.Outputs.output(table).toConsole();
+
+        int rowCount = table.getRowsList().size();
+
+        //Act
+        given()
+                .contentType(ContentType.JSON)
+                .and()
+                .body(book)
+                .when()
+                .post("/book/addBook")
+                .then()
+                .statusCode(200);
+
+        //Assert
+        table = new Table(dataSource, "BOOK");
+        int newRowCount = table.getRowsList().size();
+        org.assertj.db.output.Outputs.output(table).toConsole();
+
+        assertThat(rowCount + 1).isEqualTo(newRowCount);
+
+//        org.assertj.db.api.Assertions.assertThat(table)
+//                .column("BOOKTITLE")
+//                .value().isEqualTo("Das Ende der Welt")
+//                .value().isEqualTo("Der Anfang der Welt")
+//                .value().isEqualTo("Mamamia");
+
+        org.assertj.db.api.Assertions.assertThat(table)
+                .row(1)
+                .value().isEqualTo(1)
+                .value().isEqualTo(1)
+                .value().isEqualTo("Das Ende der Welt")
+                .value().isEqualTo(14.99)
+                .value().isEqualTo(1)
+                .value().isEqualTo(1)
+                .value().is(null);
+
+
     }
 
     @Test
